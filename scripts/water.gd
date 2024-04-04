35169d4a0a64604bf08f4feecb656d9c0de1859d
RCornejoM <rodrigo.cornejo.m@ug.uchile.cl>
2024-04-03 22:20:37 -0300

    added waves around rocks

--- scripts/water.gd
+++ scripts/water.gd
@@ -8,3 +8,5 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	self.material.set_shader_parameter("size",self.scale*self.texture.get_size())
+	self.material.set_shader_parameter("as",self.scale)
+	self.material.set_shader_parameter("waterSpeed",Vector2(0.072,0.1))
