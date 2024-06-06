cc485ae01523ac1c4bf76b9b1e313ce291e56807
RCornejoM <rodrigo.cornejo.m@ug.uchile.cl>
2024-06-06 16:27:35 -0400

    Fixed camera limits and added black holes

--- scripts/water.gd
+++ scripts/water.gd
@@ -9,9 +9,14 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var screenCenter = get_viewport().get_visible_rect().size/2;
-	self.position = camera_2d.global_position-screenCenter;
+	var screenSize =  get_viewport().get_visible_rect().size
+	var screenCenter = screenSize/2
+
+	self.position.x = clamp(camera_2d.global_position.x,camera_2d.limit_left,camera_2d.limit_right-screenSize.x);
+	self.position.y = clamp(camera_2d.global_position.y,camera_2d.limit_top,camera_2d.limit_bottom-screenSize.y);
+	
+	self.position -= screenCenter;
 	self.material.set_shader_parameter("cameraPosition",self.position)
 	self.material.set_shader_parameter("screenSize",self.scale*self.texture.get_size())
-	self.material.set_shader_parameter("waterSpeed",Vector2(0.072,0.1))
-	self.material.set_shader_parameter("gridSize",70)
+	self.material.set_shader_parameter("waterSpeed",Vector2(0.072,0.03))
+
