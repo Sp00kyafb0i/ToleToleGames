0eea95291e837c36e6d5a61b1a5f7ef8ee51d1a4
RCornejoM <rodrigo.cornejo.m@ug.uchile.cl>
2024-07-09 18:40:38 -0400

    Added the logo to the login screen

--- scripts/water.gd
+++ scripts/water.gd
@@ -1,29 +1,35 @@
 extends Node
 @onready var camera_2d = $"../Camera2D"
-@onready var boat = $"../Boat"
+@export var boat:boat
 var value:float = 0.0;
+@export var escalaGlobal:float;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	boat.healthChanged.connect(update)
-	update()
+	if (is_instance_valid(boat)):
+		boat.healthChanged.connect(update)
+		update()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var screenSize =  get_viewport().get_visible_rect().size
 	var screenCenter = screenSize/2
-	
-	camera_2d.global_position.x = boat.global_position.x
-
-	self.position.x = clamp(camera_2d.global_position.x,camera_2d.limit_left,camera_2d.limit_right-screenSize.x);
-	self.position.y = clamp(camera_2d.global_position.y,camera_2d.limit_top,camera_2d.limit_bottom-screenSize.y);
-	
+	if (is_instance_valid(boat)):
+		camera_2d.global_position.x = boat.global_position.x
+		self.position.x = clamp(camera_2d.global_position.x,camera_2d.limit_left,camera_2d.limit_right-screenSize.x);
+		self.position.y = clamp(camera_2d.global_position.y,camera_2d.limit_top,camera_2d.limit_bottom-screenSize.y);
+	else:
+		self.position.x = 0
+		self.position.y = 0
 	self.position -= screenCenter;
 	self.material.set_shader_parameter("cameraPosition",self.position)
 	self.material.set_shader_parameter("screenSize",self.scale*self.texture.get_size())
 	self.material.set_shader_parameter("waterSpeed",Vector2(0.072,0.03))
 	self.material.set_shader_parameter("damage",value)
+	self.material.set_shader_parameter("escalaGlobal",escalaGlobal)
+	
 
 
 func update():
-	value = 1.0 - float(boat.healthPoints)/boat.maxHp
+	if (is_instance_valid(boat)):
+		value = 1.0 - float(boat.healthPoints)/boat.maxHp
